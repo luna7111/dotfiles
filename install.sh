3f082b0b23307ffb4a5b1036a1751933365e5a68
luna <luna7111@tuta.io>
2025-07-17 17:48:18 +0200

    Updated install script to add symlink to .vim folder and execute YouCompleteMe install python script

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
 
 
+
 read -p "Before installing, make sure you are executing install.sh from inside the reposirory parent directory (./install.sh, not dotfiles/install.sh or so) (y/N)" confirmation
 
 if [ -z $confirmation ] || [ $confirmation != "y" ]; then
     echo abort.
     exit 0
 fi
+
+
 
 target=~/.vimrc
 src=${PWD}/vim/.vimrc
@@ -25,6 +28,33 @@ fi
 
 
 
+target=~/.vim
+src=${PWD}/vim/.vim
+if [ -d $target ] || [ -L $target ]; then
+    read -p "Directory $target already exists, overwrite? (y/N): " confirmation
+    if [ ! -z $confirmation ] && [ $confirmation = "y" ]; then
+        echo "Overwriting directory."
+        rm -rf $target
+        ln -s $src $target
+    else
+        echo "Didn't overwrite directory."
+    fi
+else
+    ln -s $src $target
+fi
+
+
+
+read -p "Do you want to install YouCompleteMe server side (YouCompleteMe/install.py)? (y/N):" confirmation
+if [ $confirmation = "y" ]; then
+    echo "Executing install.py"
+    python3 vim/.vim/pack/YouCompleteMe/opt/YouCompleteMe/install.py
+else
+    echo Skipping.
+fi
+
+
+
 target=~/.zshrc
 src=${PWD}/zsh/.zshrc
 if [ -f $target ] || [ -L $target ]; then
@@ -39,3 +69,4 @@ if [ -f $target ] || [ -L $target ]; then
 else
     ln -s $src $target
 fi
+
